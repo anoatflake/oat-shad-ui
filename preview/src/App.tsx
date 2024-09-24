394f6cf1e22fe89327fad08f0da7fa1fa9c70f2e
flockan71117 <flockan71117@th-nuernberg.de>
2024-09-24 20:31:32 +0200

    added preview project

---
+++ preview/src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "./components/theme-provider";
+import { ThemeToggle } from "./components/theme-toggle";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ThemeToggle />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
