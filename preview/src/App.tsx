9a6cf223d574ae9a0d62119e85d8f30ee8c739b6
flockan71117 <flockan71117@th-nuernberg.de>
2024-09-29 17:53:44 +0200

    preview setup

--- preview/src/App.tsx
+++ preview/src/App.tsx
@@ -1,11 +1,32 @@
+import ColorPreview from "./components/preview/colors";
+import AccordionPreview from "./components/preview/ui/accordion";
+import AlertPreview from "./components/preview/ui/alert";
+import BadgePreview from "./components/preview/ui/badge";
+import ButtonPreview from "./components/preview/ui/button";
+import CalenderPreview from "./components/preview/ui/calender";
+import CardPreview from "./components/preview/ui/card";
 import { ThemeProvider } from "./components/theme-provider";
 import { ThemeToggle } from "./components/theme-toggle";
 
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ThemeToggle />
+      <ThemeProvider defaultTheme="dark" storageKey="oat-shad-ui-theme">
+        <div className="flex flex-col items-center">
+          <header className="flex w-full max-w-screen-lg flex-row justify-around p-4">
+            <h1 className="text-3xl font-semibold">oat🌾shad🐟ui💻</h1>
+            <ThemeToggle />
+          </header>
+          <div className="flex flex-col gap-4">
+            <ColorPreview />
+            <AccordionPreview />
+            <AlertPreview />
+            <BadgePreview />
+            <ButtonPreview />
+            <CalenderPreview />
+            <CardPreview />
+          </div>
+        </div>
       </ThemeProvider>
     </>
   );
