fb04cf6a893db428eb0390eca2c89574fa157815
flockan71117 <flockan71117@th-nuernberg.de>
2024-09-29 17:43:57 +0200

    added custom colors

---
+++ preview/src/components/preview/colors.tsx
@@ -0,0 +1,166 @@
+const ColorPreview = () => {
+  return (
+    <div className="flex flex-col gap-2 rounded border p-4">
+      <h2>Color Palatte</h2>
+      <div className="flex flex-col items-center gap-4 p-12">
+        <div className="flex gap-4">
+          <span className="h-8 w-8 bg-slate-50"></span>
+          <span className="h-8 w-8 bg-slate-100"></span>
+          <span className="h-8 w-8 bg-slate-200"></span>
+          <span className="h-8 w-8 bg-slate-300"></span>
+          <span className="h-8 w-8 bg-slate-400"></span>
+          <span className="h-8 w-8 bg-slate-500"></span>
+          <span className="h-8 w-8 bg-slate-600"></span>
+          <span className="h-8 w-8 bg-slate-700"></span>
+          <span className="h-8 w-8 bg-slate-800"></span>
+          <span className="h-8 w-8 bg-slate-900"></span>
+          <span className="h-8 w-8 bg-slate-950"></span>
+        </div>
+        {/* <div className="flex gap-4">
+    <span className="h-8 w-8 bg-emerald-50"></span>
+    <span className="h-8 w-8 bg-emerald-100"></span>
+    <span className="h-8 w-8 bg-emerald-200"></span>
+    <span className="h-8 w-8 bg-emerald-300"></span>
+    <span className="h-8 w-8 bg-emerald-400"></span>
+    <span className="h-8 w-8 bg-emerald-500"></span>
+    <span className="h-8 w-8 bg-emerald-600"></span>
+    <span className="h-8 w-8 bg-emerald-700"></span>
+    <span className="h-8 w-8 bg-emerald-800"></span>
+    <span className="h-8 w-8 bg-emerald-900"></span>
+    <span className="h-8 w-8 bg-emerald-950"></span>
+  </div> */}
+        <div className="flex gap-4">
+          <span className="bg-teal_background-50 h-8 w-8"></span>
+          <span className="bg-teal_background-100 h-8 w-8"></span>
+          <span className="bg-teal_background-200 h-8 w-8"></span>
+          <span className="bg-teal_background-300 h-8 w-8"></span>
+          <span className="bg-teal_background-400 h-8 w-8"></span>
+          <span className="bg-teal_background-500 h-8 w-8"></span>
+          <span className="bg-teal_background-600 h-8 w-8"></span>
+          <span className="bg-teal_background-700 h-8 w-8"></span>
+          <span className="bg-teal_background-800 h-8 w-8"></span>
+          <span className="bg-teal_background-900 h-8 w-8"></span>
+          <span className="bg-teal_background-950 h-8 w-8"></span>
+        </div>
+        {/* <div className="flex gap-4">
+    <span className="h-8 w-8 bg-aquamarine-50"></span>
+    <span className="h-8 w-8 bg-aquamarine-100"></span>
+    <span className="h-8 w-8 bg-aquamarine-200"></span>
+    <span className="h-8 w-8 bg-aquamarine-300"></span>
+    <span className="h-8 w-8 bg-aquamarine-400"></span>
+    <span className="h-8 w-8 bg-aquamarine-500"></span>
+    <span className="h-8 w-8 bg-aquamarine-600"></span>
+    <span className="h-8 w-8 bg-aquamarine-700"></span>
+    <span className="h-8 w-8 bg-aquamarine-800"></span>
+    <span className="h-8 w-8 bg-aquamarine-900"></span>
+    <span className="h-8 w-8 bg-aquamarine-950"></span>
+  </div> */}
+        <div className="flex gap-4">
+          <span className="bg-skin-50 h-8 w-8"></span>
+          <span className="bg-skin-100 h-8 w-8"></span>
+          <span className="bg-skin-200 h-8 w-8"></span>
+          <span className="bg-skin-300 h-8 w-8"></span>
+          <span className="bg-skin-400 h-8 w-8"></span>
+          <span className="bg-skin-500 h-8 w-8"></span>
+          <span className="bg-skin-600 h-8 w-8"></span>
+          <span className="bg-skin-700 h-8 w-8"></span>
+          <span className="bg-skin-800 h-8 w-8"></span>
+          <span className="bg-skin-900 h-8 w-8"></span>
+          <span className="bg-skin-950 h-8 w-8"></span>
+        </div>
+        {/* <div className="flex gap-4">
+    <span className="h-8 w-8 bg-portage-50"></span>
+    <span className="h-8 w-8 bg-portage-100"></span>
+    <span className="h-8 w-8 bg-portage-200"></span>
+    <span className="h-8 w-8 bg-portage-300"></span>
+    <span className="h-8 w-8 bg-portage-400"></span>
+    <span className="h-8 w-8 bg-portage-500"></span>
+    <span className="h-8 w-8 bg-portage-600"></span>
+    <span className="h-8 w-8 bg-portage-700"></span>
+    <span className="h-8 w-8 bg-portage-800"></span>
+    <span className="h-8 w-8 bg-portage-900"></span>
+    <span className="h-8 w-8 bg-portage-950"></span>
+  </div> */}
+        <div className="flex gap-4">
+          <span className="bg-blood_shot_eyes-50 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-100 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-200 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-300 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-400 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-500 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-600 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-700 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-800 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-900 h-8 w-8"></span>
+          <span className="bg-blood_shot_eyes-950 h-8 w-8"></span>
+        </div>
+        {/* <div className="flex gap-4">
+    <span className="h-8 w-8 bg-persian_pink-50"></span>
+    <span className="h-8 w-8 bg-persian_pink-100"></span>
+    <span className="h-8 w-8 bg-persian_pink-200"></span>
+    <span className="h-8 w-8 bg-persian_pink-300"></span>
+    <span className="h-8 w-8 bg-persian_pink-400"></span>
+    <span className="h-8 w-8 bg-persian_pink-500"></span>
+    <span className="h-8 w-8 bg-persian_pink-600"></span>
+    <span className="h-8 w-8 bg-persian_pink-700"></span>
+    <span className="h-8 w-8 bg-persian_pink-800"></span>
+    <span className="h-8 w-8 bg-persian_pink-900"></span>
+    <span className="h-8 w-8 bg-persian_pink-950"></span>
+  </div> */}
+        <div className="flex gap-4">
+          <span className="bg-eva_danger-50 h-8 w-8"></span>
+          <span className="bg-eva_danger-100 h-8 w-8"></span>
+          <span className="bg-eva_danger-200 h-8 w-8"></span>
+          <span className="bg-eva_danger-300 h-8 w-8"></span>
+          <span className="bg-eva_danger-400 h-8 w-8"></span>
+          <span className="bg-eva_danger-500 h-8 w-8"></span>
+          <span className="bg-eva_danger-600 h-8 w-8"></span>
+          <span className="bg-eva_danger-700 h-8 w-8"></span>
+          <span className="bg-eva_danger-800 h-8 w-8"></span>
+          <span className="bg-eva_danger-900 h-8 w-8"></span>
+          <span className="bg-eva_danger-950 h-8 w-8"></span>
+        </div>
+        {/* <div className="flex gap-4">
+    <span className="bg-eva_warning-50 h-8 w-8"></span>
+    <span className="h-8 w-8 bg-eva_warning-100"></span>
+    <span className="h-8 w-8 bg-eva_warning-200"></span>
+    <span className="h-8 w-8 bg-eva_warning-300"></span>
+    <span className="h-8 w-8 bg-eva_warning-400"></span>
+    <span className="h-8 w-8 bg-eva_warning-500"></span>
+    <span className="h-8 w-8 bg-eva_warning-600"></span>
+    <span className="h-8 w-8 bg-eva_warning-700"></span>
+    <span className="h-8 w-8 bg-eva_warning-800"></span>
+    <span className="h-8 w-8 bg-eva_warning-900"></span>
+    <span className="bg-eva_warning-950 h-8 w-8"></span>
+  </div> */}
+        {/* <div className="flex gap-4">
+    <span className="h-8 w-8 bg-golden_dream-50"></span>
+    <span className="h-8 w-8 bg-golden_dream-100"></span>
+    <span className="h-8 w-8 bg-golden_dream-200"></span>
+    <span className="h-8 w-8 bg-golden_dream-300"></span>
+    <span className="h-8 w-8 bg-golden_dream-400"></span>
+    <span className="h-8 w-8 bg-golden_dream-500"></span>
+    <span className="h-8 w-8 bg-golden_dream-600"></span>
+    <span className="h-8 w-8 bg-golden_dream-700"></span>
+    <span className="h-8 w-8 bg-golden_dream-800"></span>
+    <span className="h-8 w-8 bg-golden_dream-900"></span>
+    <span className="h-8 w-8 bg-golden_dream-950"></span>
+  </div> */}
+        <div className="flex gap-4">
+          <span className="bg-gold-50 h-8 w-8"></span>
+          <span className="bg-gold-100 h-8 w-8"></span>
+          <span className="bg-gold-200 h-8 w-8"></span>
+          <span className="bg-gold-300 h-8 w-8"></span>
+          <span className="bg-gold-400 h-8 w-8"></span>
+          <span className="bg-gold-500 h-8 w-8"></span>
+          <span className="bg-gold-600 h-8 w-8"></span>
+          <span className="bg-gold-700 h-8 w-8"></span>
+          <span className="bg-gold-800 h-8 w-8"></span>
+          <span className="bg-gold-900 h-8 w-8"></span>
+          <span className="bg-gold-950 h-8 w-8"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ColorPreview;
--- preview/tailwind.config.js
+++ preview/tailwind.config.js
@@ -1,62 +1,149 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+        blood_shot_eyes: {
+          50: "hsl(var(--blood_shot_eyes-50))",
+          100: "hsl(var(--blood_shot_eyes-100))",
+          200: "hsl(var(--blood_shot_eyes-200))",
+          300: "hsl(var(--blood_shot_eyes-300))",
+          400: "hsl(var(--blood_shot_eyes-400))",
+          500: "hsl(var(--blood_shot_eyes-500))",
+          600: "hsl(var(--blood_shot_eyes-600))",
+          700: "hsl(var(--blood_shot_eyes-700))",
+          800: "hsl(var(--blood_shot_eyes-800))",
+          900: "hsl(var(--blood_shot_eyes-900))",
+          950: "hsl(var(--blood_shot_eyes-950))",
+        },
+        eva_danger: {
+          50: "hsl(var(--eva_danger-50))",
+          100: "hsl(var(--eva_danger-100))",
+          200: "hsl(var(--eva_danger-200))",
+          300: "hsl(var(--eva_danger-300))",
+          400: "hsl(var(--eva_danger-400))",
+          500: "hsl(var(--eva_danger-500))",
+          600: "hsl(var(--eva_danger-600))",
+          700: "hsl(var(--eva_danger-700))",
+          800: "hsl(var(--eva_danger-800))",
+          900: "hsl(var(--eva_danger-900))",
+          950: "hsl(var(--eva_danger-950))",
+        },
+        teal_background: {
+          50: "hsl(var(--teal_background-50))",
+          100: "hsl(var(--teal_background-100))",
+          200: "hsl(var(--teal_background-200))",
+          300: "hsl(var(--teal_background-300))",
+          400: "hsl(var(--teal_background-400))",
+          500: "hsl(var(--teal_background-500))",
+          600: "hsl(var(--teal_background-600))",
+          700: "hsl(var(--teal_background-700))",
+          800: "hsl(var(--teal_background-800))",
+          900: "hsl(var(--teal_background-900))",
+          950: "hsl(var(--teal_background-950))",
+        },
+        skin: {
+          50: "hsl(var(--skin-50))",
+          100: "hsl(var(--skin-100))",
+          200: "hsl(var(--skin-200))",
+          300: "hsl(var(--skin-300))",
+          400: "hsl(var(--skin-400))",
+          500: "hsl(var(--skin-500))",
+          600: "hsl(var(--skin-600))",
+          700: "hsl(var(--skin-700))",
+          800: "hsl(var(--skin-800))",
+          900: "hsl(var(--skin-900))",
+          950: "hsl(var(--skin-950))",
+        },
+        gold: {
+          50: "hsl(var(--gold-50))",
+          100: "hsl(var(--gold-100))",
+          200: "hsl(var(--gold-200))",
+          300: "hsl(var(--gold-300))",
+          400: "hsl(var(--gold-400))",
+          500: "hsl(var(--gold-500))",
+          600: "hsl(var(--gold-600))",
+          700: "hsl(var(--gold-700))",
+          800: "hsl(var(--gold-800))",
+          900: "hsl(var(--gold-900))",
+          950: "hsl(var(--gold-950))",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
