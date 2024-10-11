6c00e797beda671752d5fbb741ce76f3f851f76e
flockan71117 <flockan71117@th-nuernberg.de>
2024-10-11 11:05:12 +0200

    changes wip (worked with shadcn and next...)

--- preview/src/components/preview/ui/calender.tsx
+++ preview/src/components/preview/ui/calender.tsx
@@ -1,12 +1,29 @@
 import { Calendar } from "@/components/ui/calendar";
 import { useState } from "react";
+import { DateRange } from "react-day-picker";
 
 const CalenderPreview = () => {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [multiDate, setMultiDate] = useState<Date[] | undefined>([
+    new Date(),
+    new Date("10-08-2024"),
+  ]);
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: new Date("10-01-2024"),
+    to: new Date(),
+  });
   return (
     <div className="flex flex-col gap-2 rounded border p-4">
       <h2>Calender</h2>
-      <Calendar selected={date} onSelect={setDate} />
+      <div className="flex flex-row gap-4">
+        <Calendar mode="single" selected={date} onSelect={setDate} />
+        <Calendar
+          mode="multiple"
+          selected={multiDate}
+          onSelect={setMultiDate}
+        />
+        <Calendar mode="range" selected={dateRange} onSelect={setDateRange} />
+      </div>
     </div>
   );
 };
--- preview/src/components/ui/calendar.tsx
+++ preview/src/components/ui/calendar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
@@ -16,56 +18,52 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={cn("relative p-3", className)}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-0 sm:space-y-0",
         month: "space-y-4",
-        month_caption:
-          "flex justify-center pt-1 relative items-center text-sm font-medium",
+        month_caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         button_previous: cn(
-          buttonVariants({ variant: "outline", size: "xs" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 rounded-full",
+          buttonVariants({ variant: "outline" }),
+          "absolute top-4 left-4 h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
         ),
         button_next: cn(
-          buttonVariants({ variant: "outline", size: "xs" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 rounded-full",
+          buttonVariants({ variant: "outline" }),
+          "absolute top-4 right-4 h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
         ),
         month_grid: "w-full border-collapse space-y-1",
         weekdays: "flex",
         weekday:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] mx-0.5",
-        week: "flex w-full mt-1",
-        day: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 mx-0.5",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+        week: "flex w-full mt-2",
+        day: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].range-end)]:rounded-r-md [&:has([aria-selected].outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day_button: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-8 w-8 p-0 font-normal aria-selected:opacity-100",
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100",
         ),
-        range_end: "day-range-end",
-        selected: cn(
-          buttonVariants({ variant: "primary", hue: "none" }),
-          "h-8 w-8 p-0 hover:opacity-100 rounded-full ",
-        ),
-        today: "bg-accent text-accent-foreground",
+        range_end: "range-end",
+        selected:
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground rounded-md",
+        today: "bg-accent text-accent-foreground rounded-md",
         outside:
-          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+          "outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
         disabled: "text-muted-foreground opacity-50",
         range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "aria-selected:bg-accent aria-selected:text-accent-foreground rounded-none",
         hidden: "invisible",
         ...classNames,
       }}
-      // components={
-      //   {
-      //     IconLeft: ({ ...props }) => (
-      //       <ArrowLeft strokeWidth={3} className="h-4 w-4" />
-      //     ),
-      //     IconRight: ({ ...props }) => (
-      //       <ArrowRight strokeWidth={3} className="h-4 w-4" />
-      //     ),
-      //   }
-      // }
+      components={{
+        Chevron: (props) => {
+          if (props.orientation === "left") {
+            return <ArrowLeft {...props} className="h-4 w-4" />;
+          }
+          return <ArrowRight {...props} className="h-4 w-4" />;
+        },
+      }}
+      {...props}
     />
   );
 }
