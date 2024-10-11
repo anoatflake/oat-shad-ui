83cb9acae4c717481f4f526690cc3a5c9cc2364d
flockan71117 <flockan71117@th-nuernberg.de>
2024-10-11 11:05:12 +0200

    changes wip (worked with shadcn and next...)

--- preview/src/components/preview/ui/calender.tsx
+++ preview/src/components/preview/ui/calender.tsx
@@ -3,29 +3,24 @@ import { useState } from "react";
 import { DateRange } from "react-day-picker";
 
 const CalenderPreview = () => {
-  const [singleDate, setSingleDate] = useState<Date | undefined>(new Date());
-  const [multipleDates, setMultipleDates] = useState<Date[] | undefined>([
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [multiDate, setMultiDate] = useState<Date[] | undefined>([
     new Date(),
-    new Date("10-01-2024"),
+    new Date("10-08-2024"),
   ]);
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
     from: new Date("10-01-2024"),
     to: new Date(),
   });
-
   return (
     <div className="flex flex-col gap-2 rounded-lg border p-4">
       <h2>Calender</h2>
-      <div className="flex flex-row flex-wrap items-center justify-center gap-4 md:flex-row">
-        <Calendar
-          mode="single"
-          selected={singleDate}
-          onSelect={setSingleDate}
-        />
+      <div className="flex flex-row gap-4">
+        <Calendar mode="single" selected={date} onSelect={setDate} />
         <Calendar
           mode="multiple"
-          selected={multipleDates}
-          onSelect={setMultipleDates}
+          selected={multiDate}
+          onSelect={setMultiDate}
         />
         <Calendar mode="range" selected={dateRange} onSelect={setDateRange} />
       </div>
