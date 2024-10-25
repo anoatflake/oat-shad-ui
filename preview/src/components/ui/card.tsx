d06c531e351caf23ca7e112e74cd46b3b959ea8c
flockan71117 <flockan71117@th-nuernberg.de>
2024-10-25 10:10:22 +0200

    update select styles

--- preview/src/components/ui/card.tsx
+++ preview/src/components/ui/card.tsx
@@ -38,7 +38,7 @@ const Card = React.forwardRef<HTMLDivElement, CardProps>(
   ({ className, hue, ...props }: CardProps, ref) => (
     <div className={cn(cardVariants({ hue }), className)}>
       <div ref={ref} {...props} />
-      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#4f4f4f2e_2px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_2px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+      {/* <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#4f4f4f2e_2px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_2px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" /> */}
     </div>
   ),
 );
