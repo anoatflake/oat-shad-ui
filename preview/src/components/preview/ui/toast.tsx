bcc0388a515fc4a9c1e08b68d3f9cfbc475b46c2
flockan71117 <flockan71117@th-nuernberg.de>
2024-10-27 14:09:44 +0100

    scalloped cards and toasts

--- preview/src/components/preview/ui/toast.tsx
+++ preview/src/components/preview/ui/toast.tsx
@@ -1,8 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/hooks/use-toast";
+
 const ToastPreview = () => {
+  const { toast } = useToast();
+
   return (
     <div className="flex flex-col gap-2 rounded-lg border p-4">
       <h2>Toast</h2>
-      TODO: COPY EXAMPLE
+      <div className="flex justify-center gap-4">
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast({
+              title: "Scheduled: Catch up ",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+              action: (
+                <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+              ),
+            });
+          }}
+        >
+          default variant
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast({
+              variant: "destructive",
+              title: "Scheduled: Catch up ",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+              action: (
+                <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+              ),
+            });
+          }}
+        >
+          destructive variant
+        </Button>
+      </div>
     </div>
   );
 };
--- preview/src/components/ui/sonner.tsx
+++ preview/src/components/ui/sonner.tsx
@@ -1,29 +1,51 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { cn } from "@/lib/utils";
+import {
+  CircleCheckBig,
+  CircleX,
+  Info,
+  RotateCw,
+  TriangleAlert,
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+import { buttonVariants } from "./button";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      visibleToasts={4}
       className="toaster group"
       toastOptions={{
+        unstyled: true,
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+            "scalloped toast group w-full p-6 md:max-w-[420px] first:bg-white [&:nth-child(2)]:bg-slate-100 [&:nth-child(3)]:bg-slate-200 [&:nth-child(4)]:bg-slate-300 bg-slate-300 dark:first:bg-black dark:[&:nth-child(2)]:bg-stone-950 dark:[&:nth-child(3)]:bg-stone-900 dark:[&:nth-child(4)]:bg-stone-800 dark:bg-stone-800 flex items-center gap-4",
+          title: "text-md",
+          description: "group-[.toast]:text-muted-foreground text-sm",
+          actionButton: cn(
+            buttonVariants({ variant: "secondary", size: "sm" }),
+          ),
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
       }}
+      icons={{
+        success: <CircleCheckBig className="text-green-600" />,
+        info: <Info />,
+        warning: (
+          <TriangleAlert className="fill-gold-400 dark:fill-black dark:text-gold-400" />
+        ),
+        error: <CircleX className="text-red-500" />,
+        loading: <RotateCw className="animate-spin" />,
+      }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
