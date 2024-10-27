75f4bf3706e78eb3e82d56b140cea5366f97dffd
flockan71117 <flockan71117@th-nuernberg.de>
2024-10-27 15:18:16 +0100

    deployment fix

---
+++ preview/src/components/preview/ui/input.tsx
@@ -0,0 +1,17 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const InputPreview = () => {
+  return (
+    <div className="flex flex-col gap-2 rounded-lg border p-4">
+      <h2>Input</h2>
+      <div className="flex justify-center gap-4">
+        <span>
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" placeholder="Email" />
+        </span>
+      </div>
+    </div>
+  );
+};
+export default InputPreview;
